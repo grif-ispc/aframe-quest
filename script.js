f73049bd803313df2df933366801078d13feb197
Ferran Julia <ferran.julia@gmail.com>
2020-04-30 15:42:01 +0200

    develop

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
       button.addEventListener('click', ()=> {
         toast.show();
       })
+
+      let form = document.querySelector('a-rounded');
+      form.setAttribute('visible', false);
+
+      setTimeout(function(){ 
+        let form = document.querySelector('a-rounded');
+        form.setAttribute('visible', true);
+        let video = document.getElementById('video');
+    video.currentTime = 0;
+    video.pause()
+      }, 10000);
